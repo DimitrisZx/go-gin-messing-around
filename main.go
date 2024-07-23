cda052d6bb7a7e8acabb92e5e81d6d281ae1fca3
DimitrisZar <dimitriszarahanis@gmail.com>
2024-07-23 19:14:21 +0300

    add http handlers

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/", indexHandler)
+	app.Post("/", postHandler)
+	app.Put("/", putHandler)
+	app.Delete("/", deleteHandler)
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -16,4 +22,20 @@ func main() {
 	}
 
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
+
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func deleteHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
 }
