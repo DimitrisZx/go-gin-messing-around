b655a5a1c78c12e4e17cd92de0fbe9e9a492e3e3
DimitrisZar <dimitriszarahanis@gmail.com>
2024-07-24 09:53:22 +0300

    changes

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
 
 func main() {
+	connStr := "postgres://<username>:<password>@<database_ip>:5432/todos?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 
-	app.Get("/", indexHandler)
-	app.Post("/", postHandler)
-	app.Put("/", putHandler)
-	app.Delete("/", deleteHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+	app.Put("/", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+	app.Delete("/", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
 
 	port := os.Getenv("PORT")
 
@@ -24,18 +40,18 @@ func main() {
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
 }
 
-func indexHandler(c *fiber.Ctx) error {
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func postHandler(c *fiber.Ctx) error {
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func putHandler(c *fiber.Ctx) error {
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
 
-func deleteHandler(c *fiber.Ctx) error {
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello, World!")
 }
